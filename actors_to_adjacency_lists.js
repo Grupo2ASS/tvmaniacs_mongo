a526d6f17edd2003bc22d9ef3ff3141bda391eb3
Ignacio Fuenzalida <nacho_fuenzalida@hotmail.com>
2013-10-14 10:29:52 -0300

    changing script

--- actors_to_adjacency_lists.js
+++ actors_to_adjacency_lists.js
@@ -1,7 +1,14 @@
-functio printResult(r){
+function printResult(r){
   print(tojson(r));
 };
 
 conn = new Mongo();
-db = conn.getDB("tvdb");
-db.actors.find().forEach(printResult);
+db = conn.getDB("mydb");
+var c = db.series.find( {}, { _id: 0, cast: 1 } );
+var series_cast = c.toArray();
+var a = tojson(series_cast[1]);
+
+print(a);
+
+print("algo: "+a.cast[0].name);
+
