c211e47381604942af3923d9838cd1b3c51867bd
Ignacio Fuenzalida <nacho_fuenzalida@hotmail.com>
2013-10-20 21:39:49 -0300

    best approach to get actors

--- actors_to_adjacency_lists.js
+++ actors_to_adjacency_lists.js
@@ -4,11 +4,43 @@ function printResult(r){
 
 conn = new Mongo();
 db = conn.getDB("mydb");
-var c = db.series.find( {}, { _id: 0, cast: 1 } );
+var query = db.series.find( {}, { _id: 0, cast: 1 } );
+var array = new Array();
+query.forEach(function(o){array.push(o)});
+
+var bacon = array.filter(function(x) { return x["name"]== "Madeleine Stowe"; })[0];
+print("aca: "+bacon);
+
+
+/*
 var series_cast = c.toArray();
 var a = tojson(series_cast[1]);
-
 print(a);
-
 print("algo: "+a.cast[0].name);
+*/
+
+
+map = function() {
+  for(var i in this.cast){
+    key = { cast: this.cast[i] };
+    value = 0;
+    emit(key,value);
+  }
+}
+
+reduce = function(key, values) {
+  actor_list = { actors: [] };
+  for(var i in values) {
+    actor_list.series = values[i].series.concat(actor_list.actors);
+  }
+  return actor_list;
+}
+
+ printjson(db.series.mapReduce(map, reduce, { out: "test"}));
+ db.test.find().forEach(printjson);
+
+
+
+
+
 
