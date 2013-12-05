607e33d169186d4ada0d412fc0a4702d8663a60e
Felipe Alamos <fhalamos@uc.cl>
2013-12-05 11:52:53 -0300

    Funcionando bacon_number!

--- six_degrees_of_tocino.js
+++ six_degrees_of_tocino.js
@@ -78,7 +78,7 @@ var db = conn.getDB("tvdb");
 var query = db.adjacency_lists.find();
 var graph = new Array();
 query.forEach(function(o){graph.push(o)});
-var bacon = graph.filter(function(x) { return x["name"]== "4263213"; })[0]; //0000102 es el imdb_id de Kevin Bacon 1 should be Kevin Bacon
+var bacon = graph.filter(function(x) { return x["name"]== "0000102"; })[0]; //0000102 es el imdb_id de Kevin Bacon 1 should be Kevin Bacon
 //printjson(bacon);
 bfs(graph, bacon);
 
--- update_actors.js
+++ update_actors.js
@@ -2,15 +2,11 @@ var conn = new Mongo();
 var db = conn.getDB("tvdb");
 var lists = db.adjacency_lists.find();
 lists.forEach(function(l){
-	db.actors.update( { "name": l["name"] },
+	db.actors.update( { "imdb_id": l["name"] },
                  {
-                   $set: { "degree": l["degree"] }
+                   $set: { "bacon_number": l["degree"] }
                    
                  } )
-	print("se actualizo "+l["name"]+" con degree "+l["degree"]);
-	//lo raro y lo q falta esq no se actualiza db.actors en el campo degree
-	//ya que los actors no tienen degree!
-	//le agregue el degree a los actors en models.js pero aun asi no hay cambios
-	//hay q ver tmb de dnd django obtiene el degree pa publicarlo
+	print("se actualizo el actor "+l["name"]+" con bacon number "+l["degree"]);
 
 });
