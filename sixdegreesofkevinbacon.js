0e6cfbd0c89573805b28cbf2b2c95f99e5894e3e
Felipe Alamos <fhalamos@uc.cl>
2013-12-04 21:06:52 -0300

    Prueba1

--- sixdegreesofkevinbacon.js
+++ sixdegreesofkevinbacon.js
@@ -1,3 +1,8 @@
 load('actors_to_adjacency_lists.js');
+print("Listo actors_to_adjacency_lists");
+
 load('six_degrees_of_tocino.js');
+print("Listo six_degrees_of_tocino");
+
 load('update_actors.js');
+print("Listo actor.js");
